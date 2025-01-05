107c9abe286a7eaee8c2dd8484ac9e51cd91a09b
joewiltshire-uniform <homeworldrocks@yahoo.co.uk>
2025-01-05 14:44:14 +0000

    prepare for deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = "diabetes-material-tracker-page"; // Your GitHub repo name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: isProd ? `/${repoName}/` : "",
+  basePath: isProd ? `/${repoName}` : "",
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
+  /* Add other custom config options here */
 };
 
 export default nextConfig;
