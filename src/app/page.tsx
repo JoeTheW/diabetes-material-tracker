df11439015e7dee78529afa169b7f4f6162f5d05
joewiltshire-uniform <homeworldrocks@yahoo.co.uk>
2025-01-07 10:53:10 +0000

    colour adjust

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
         <h1 className="text-3xl font-bold">Home</h1>
         <p className="mt-4 text-lg">Welcome to the Supply tracker. A tool to help you keep track of your supplies and usage dates.</p>
 
-        <div tabIndex={0} className="collapse collapse-plus border-warning bg-warning-200 border mt-4 max-w-full sm:max-w-[50%]">
+        <div tabIndex={0} className="collapse collapse-plus border-primary border mt-4 max-w-full sm:max-w-[50%]">
           <input type="checkbox" />
-          <div className="collapse-title text-sm font-medium text-warning">
+          <div className="collapse-title text-sm font-medium text-primary">
             Click to view additional information
           </div>
-          <div className="collapse-content text-sm text-warning">
+          <div className="collapse-content text-sm text-primary">
             <p>Your information, items and settings are stored locally on this device and browser.<br></br>They will not persist across different devices or browsers.</p>
           </div>
         </div>
