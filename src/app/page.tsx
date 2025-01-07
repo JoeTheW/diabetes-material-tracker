07e10fb4849f685add0deabee47bdab08a4bf608
joewiltshire-uniform <homeworldrocks@yahoo.co.uk>
2025-01-07 12:52:53 +0000

    adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,10 +12,10 @@ export default function Index() {
   }, [router]);
 
   return (
-    <div className="grid items-center p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid items-center px-8 font-[family-name:var(--font-geist-sans)]">
       <main className="">
-        <p className="mt-4 text-lg">Redirecting you to the home page...</p>
-        <p className="mt-4 text-lg">If you are not redirected please click <a href="/home" className="text-primary font-bold">here</a>.</p>
+        <p className="text-lg">Redirecting you to the home page...</p>
+        <p className="mt-2 text-lg">If you are not redirected please click <a href="/home" className="text-primary font-bold">here</a>.</p>
       </main>
     </div>
   );
