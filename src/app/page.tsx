8d75e0602ee24c97d24003e48d59ddb9c7cea652
joewiltshire-uniform <homeworldrocks@yahoo.co.uk>
2025-01-07 12:42:33 +0000

    redirect copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Index() {
-  return;
+  return (
+    <div className="grid items-center p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <main className="">
+        <p className="mt-4 text-lg">Redirecting you to the home page...</p>
+        <p>If you are not redirected please click <a href="/home" className="text-primary font-bold">here</a>.</p>
+      </main>
+    </div>
+  );
 }
