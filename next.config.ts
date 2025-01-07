3bcfa1c03bacd9efc5a76e60a026d4cd47fc9d98
joewiltshire-uniform <homeworldrocks@yahoo.co.uk>
2025-01-07 12:50:25 +0000

    adjust for gh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === "production";
-const repoName = "diabetes-material-tracker"; // Your GitHub repo name
+const repoName = "supply-tracker"; // Your GitHub repo name
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -9,16 +9,6 @@ const nextConfig: NextConfig = {
   basePath: isProd ? `/${repoName}` : "",
   images: {
     unoptimized: true, // Required for GitHub Pages
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true,
-      },
-    ];
   },
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+'use client';
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Index() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, [router]);
+
   return (
     <div className="grid items-center p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="">
         <p className="mt-4 text-lg">Redirecting you to the home page...</p>
-        <p>If you are not redirected please click <a href="/home" className="text-primary font-bold">here</a>.</p>
+        <p className="mt-4 text-lg">If you are not redirected please click <a href="/home" className="text-primary font-bold">here</a>.</p>
       </main>
     </div>
   );
